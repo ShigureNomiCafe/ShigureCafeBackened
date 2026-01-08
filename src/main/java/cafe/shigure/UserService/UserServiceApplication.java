c2b8c33e6ad23e2ee4436e4a7f0965f56ddfda0b
初時雨🍥 <Hatsushigure.c@gmail.com>
2026-01-08 19:35:24 +0800

    security(env): externalize sensitive credentials to .env using dotenv-java

--- src/main/java/cafe/shigure/UserService/UserServiceApplication.java
+++ src/main/java/cafe/shigure/UserService/UserServiceApplication.java
@@ -1,5 +1,6 @@
 package cafe.shigure.UserService;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,6 +10,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
